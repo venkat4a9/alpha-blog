e4b2a354bd0bd5b76bcf8c4305ecb3f9b56eee08
cgm <cgm.cgm@gmail.com>
2017-04-15 15:04:55 +0530

    Fix create,add flash and error message,create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,16 +5,23 @@ class ArticlesController <ApplicationController
 
 	end
 	def create
-		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to article_show(@article)
+		if @article.save
+			flash[:notice] = "Article was sucessfully created"
+			
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+	def show
+		@article = Article.find(params[:id])
 	end
 
 	private
 
 		def article_params
-			params.require(:article_params).permit(:title, :description)
+			params.require(:article).permit(:title, :description)
 		end
 
 end
