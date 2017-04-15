f7ae01373f5b28812563aee9619076c91c339bbe
cgm <cgm.cgm@gmail.com>
2017-04-15 17:23:47 +0530

    Add particals and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,6 +39,15 @@ class ArticlesController <ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash[:notice] = "Article was sucessfully deleted"
+		redirect_to articles_path
+		
+	end
+
 	private
 
 		def article_params
