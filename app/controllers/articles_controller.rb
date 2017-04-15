4969fc94d565b046ad2372c0321a7f297fe4f627
cgm <cgm.cgm@gmail.com>
2017-04-15 16:44:52 +0530

    Add edit, update, show and listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController <ApplicationController
 
+	def index
+		@articles = Article.all
+
+	end
+
 	def new
 		@article = Article.new
 
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+
+	end
+	
 	def create
 		@article = Article.new(article_params)
 		if @article.save
@@ -14,6 +25,16 @@ class ArticlesController <ApplicationController
 			render 'new'
 		end
 	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice]= "Article was sucessfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
